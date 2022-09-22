a26a4b618c4f82d239d074e0804ed1e4ef4f4b60
Tiago <ztq7600@gmail.com>
2022-09-22 09:45:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <App />;
+root.render(element);
